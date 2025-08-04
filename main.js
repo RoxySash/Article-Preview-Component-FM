def663a616a3da426a48cbd39eb07e6d831a9a35
Roxane Hedge <roxanehedge@gmail.com>
2025-08-03 20:02:06 -0500

    updated the js and button colors

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const share = document.getElementById("share-btn");
 
-function toggleVisibility(select) {
-     // use function to toggle
-    document.querySelector(select).classList.toggle("hidden");
-};
+
+const shareBtns = document.querySelectorAll(".share-btn");
+const shareSectionOne = document.querySelector(".share-section-one");
+const shareSectionTwo = document.querySelector(".share-section-two");
+//
+
+
+
+shareBtns.forEach(btn => {
 
 // add click event listener 
-share.addEventListener("click", (event) => {
-  toggleVisibility("#myDiv");
-  
+btn.addEventListener("click", () => {
+ shareSectionOne.classList.toggle("hidden");
+  shareSectionTwo.classList.toggle("hidden");
 });
  
+});
